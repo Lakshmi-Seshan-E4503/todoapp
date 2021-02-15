6aaa677801c52687c31f01385c13ec4c122622cd
Lakshmi <lakshmi.seshan@freshworks.com>
2021-02-15 14:00:02 +0530

    Commit app

---
+++ microblog_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroblogApp::Application.config.secret_token = 'a8167d34bf39ca236875def51b5d5ef29c5d002e94066442da55e6fbbc4f67a80195caa99c18d220488fad8b796821b9f97cd7fd797dbe30274e3255b68a1682'
